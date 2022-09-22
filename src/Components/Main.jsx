4b99383225a0d4a59e038b3dfe36aee1ae3ec6a6
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-09-22 18:51:35 -0300

    Parte da Main terminada amigo

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,6 +3,7 @@ import * as S from '../Styles/StyleMain'
 import Bolo from '../Img/Main/BoloMain.png'
 import Pizza from '../Img/Main/PizzaMain.png'
 import Copo from '../Img/Main/CopoMain.png'
+import Colher from '../Img/Main/ImgColherMain.png'
 
 export default function Main(){
     return(
@@ -30,6 +31,18 @@ export default function Main(){
                     <p>Peanut Smoothie</p>
                 </S.Caixa>
             </S.ContainerFood>
+            <S.Section2>
+                <S.Img2 src={Colher} alt="Colher" />
+                <S.Texto2>ABOUT</S.Texto2>
+                <S.Linha3></S.Linha3>
+                <S.lorem>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt, tortor nec rhoncus dictum, lorem massa tempus sem, eu tincidunt libero velit sit amet velit. Nunc in euismod urna. Duis dapibus, elit eu eleifend tincidunt, nulla ipsum consectetur lorem, quis tempor lorem justo quis nisi. Nam interdum, nisi nec mollis sagittis, enim risus euismod nisi, quis rutrum quam augue id mauris. Pellentesque mattis hendrerit semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vestibulum nisl ante, et ultricies sapien facilisis aliquam.</S.lorem>
+            </S.Section2>
+            <S.Section3>
+                <h4>SUBSCRIBE</h4>
+                <p>Sign up for newsletter</p>
+                imput
+            </S.Section3>
         </S.ContainerMain>
+        
     )
 }
--- src/Styles/StyleMain.js
+++ src/Styles/StyleMain.js
@@ -51,3 +51,43 @@ export const Linha = styled.div`
     height: 0.5vh;
     width: 5vw;
 `
+export const Section2 = styled.section`
+margin-top:12vw;
+display:flex;
+`
+export const Img2 = styled.img`
+width:90vh;
+height:36.8vw;
+
+`
+
+export const Texto2 = styled.h2`
+position: relative;
+left: 22vw;
+top: 10vh
+
+`
+export const lorem = styled.p`
+width:30%;
+height:10px;
+font-size:13.5px;
+margin-top:13vw;
+margin-left:0vw;
+border:solid
+`
+export const Linha3 = styled.div`
+    background-color: #373737;
+    height: 0.5vh;
+    width: 4vw;
+    position:relative;
+    top:8vw;
+    left:34vh;
+
+`
+export const Section3 = styled.section`
+background-color:#DFE4DE;
+width: 99.1vw;
+height:60vh;
+
+
+`
